e82f96c7366c6373bb7f89c729a9ffeba18abfa1
Dinos <143280137+Dinos17@users.noreply.github.com>
2025-01-19 03:21:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,11 +14,14 @@ import time
 import random
 import praw
 
-# Accessing environment variables
+client_id = os.getenv("REDDIT_CLIENT_ID")
+secret = os.getenv("REDDIT_SECRET")
+user_agent = os.getenv("REDDIT_USER_AGENT")
+
 reddit = praw.Reddit(
-    client_id= os.getenv("REDDIT_CLIENT_ID")
-    client_secret= os.getenv("REDDIT_SECRET")
-    user_agent= os.getenv("REDDIT_USER_AGENT")
+    client_id=client_id,
+    client_secret=secret,
+    user_agent=user_agent
 )
 
 TOKEN = os.getenv("BOT_TOKEN")
